b16591499e81d7472e96a82bf641cbad49b9642e
Bilal El Haddadi <bilal.elhaddadi@student.vinci.be>
2020-10-21 13:11:18 +0200

    add

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,7 +1,19 @@
 <?php
 
+namespace FizzBuzz;
 
 class FizzBuzz
 {
+
+
+
+    public function count(int $int):string
+    {
+
+        return "1";
+
+
+    }
+
 
 }
--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,7 +1,30 @@
 <?php
+require "vendor/autoload.php";
+require "FizzBuzz.php";
+
+use FizzBuzz\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+
+    public function test_count_given1_shouldReturn1(){
+      // Arrange (on prepare tous ce dont on a besoin pour le test, càd les variables etc..)
+        $fizzBuzz = new FizzBuzz();
+
+        //Act (execute la fonction que l'on souhaite tester)
+        $actual = $fizzBuzz->count(1);
+
+        //Assert
+        $this->assertEquals("1",$actual);
+
+       // $this->assertTrue(true ); // raccourci pour les booleans (au lieu de mettre le assertEquals)
+
+
+
+
+      //  $this->assertEquals(true,true); // verifier que 2 valeurs sont egales  (true est bien egal a true )
+
+    }
 
 }
